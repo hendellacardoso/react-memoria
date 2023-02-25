c6c4a970079427e139d874441c73f797703098d5
hendellacardoso <hendellacardoso@gmail.com>
2023-02-25 16:21:17 -0300

    adding files to repo

---
+++ src/components/GridItem/index.tsx
@@ -0,0 +1,26 @@
+import { type } from 'os';
+import { GridItemType } from '../../types/GridItemTypes';
+import * as C from './styles';
+import b7Svg from '../../svgs/b7.svg';
+import { items } from '../../data/itens';
+
+type Props = {
+    item: GridItemType,
+    onClick: () => void;
+}
+
+export const GridItem = ( {item, onClick}: Props ) => {
+    return(
+        <C.Container 
+            showBackGround={item.permanentShown || item.shown}
+            onClick={onClick}                
+        >
+            {!item.permanentShown && !item.shown &&
+                <C.Icon src={b7Svg} alt="" opacity={.1} />
+            }
+            {(item.permanentShown || item.shown) && item.item !== null &&
+                <C.Icon src={items[item.item].icon} alt="" />
+            }            
+        </C.Container>
+    )
+}
